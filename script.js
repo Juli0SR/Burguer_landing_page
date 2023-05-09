3fffdb73ac2cb402241d36b0a19e8c7137b978ac
juli0SR <sjulio841@gmail.com>
2023-05-09 13:09:58 -0300

    burguer_landing_page

---
+++ script.js
@@ -0,0 +1,51 @@
+function MENUCLICK(){
+    const menuClick = document.querySelector('.bg header i');
+    const nav = document.querySelector('.bg header nav ul');
+
+    menuClick.addEventListener('click', clicou);
+
+    function clicou(){
+        nav.classList.toggle('ativo');
+    }    
+}
+
+MENUCLICK();
+
+function lista(){
+
+    const lista = document.querySelectorAll('.js-texto p');
+    const listaIcone = document.querySelectorAll('.js-texto i');
+    lista[0].classList.add('ativo')
+
+
+    function clicklista(index){
+        lista.forEach(item => {
+            item.classList.remove('ativo');  
+        });
+        lista[index].classList.add('ativo');
+    }
+
+    listaIcone.forEach((item, index) =>{
+        item.addEventListener('click', () => {
+            clicklista(index)
+        })
+    })
+}
+
+lista();
+
+const scroll = document.querySelectorAll('.js-scroll');
+const height = window.innerHeight * 0.6;
+scroll[0].classList.add('ativo');
+
+function mauseScroll() {
+    scroll.forEach((section) => {
+        const sectionTop = (section.getBoundingClientRect().top - height) < 0;
+        if(sectionTop){
+            section.classList.add('ativo');
+        }
+    })
+}
+
+
+window.addEventListener('scroll', mauseScroll)
